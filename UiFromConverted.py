490679b1580baaf2f1b9b5c20ba780f3a003ae39
HennaSalla <51696@edu.raseko.fi>
2024-11-14 14:59:15 +0200

    .

--- UiFromConverted.py
+++ UiFromConverted.py
@@ -1,5 +1,5 @@
 # PYSIDE6-MALLINE SOVELLUKSEN PÄÄIKKUNAN LUOMISEEN
-# KÄÄNETYSTÄ KÄYTTÖLITTYMÄTIEDOSTOSTA (MainWindow.py)
+# KÄÄNETYSTÄ KÄYTTÖLITTYMÄTIEDOSTOSTA (mainWindow_ui.py)
 # ===================================================
 
 # KIRJASTOJEN JA MODUULIEN LATAUKSET
@@ -8,7 +8,7 @@ import os # Polkumääräykset
 import sys # Käynistysargumentit
 
 from PySide6 import QtWidgets #Qt-vimpaimet
-from MainWindow import Ui_MainWindow # Käänetyn käyttöliitymän luokka
+from mainWindow_ui import Ui_MainWindow # Käänetyn käyttöliitymän luokka
 
 # Märitellään luokka, joka perii QMainWindow- ja UI_MainWindow luokan
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -18,8 +18,35 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
 
+        # Luodaan käyttöliitymä konvertoidun tiedoston perusteella MainWindown ui-ominaisuudeksi. Tämä suojaa lopun MainWindow-olion ylikirjoitukselta
+        self.ui = Ui_MainWindow()
+
         # Kutsutaan käyttöliittymän muodostusmetodia setupUi
-        self.setupUi(self)
+        self.ui.setupUi(self)
+
+        # OHJELMOIDUT SINGAALIT
+        # ---------------------
+
+        # Kun Tulosta painiketta on klikattu, kutsutaan updatePrintedLabel-metodia
+        self.ui.tulostaPushButton.clicked.connect(self.updatePrintedLabel)
+        self.ui.varoitaPushButton.clicked.connect(self.openWarning)
+
+    # OHJELMOIDUT SLOTIT
+    # ------------------
+
+    # Muutetaan tulostettuLabel:n sisältö: teksti ja väri
+    def updatePrintedLabel(self):
+        self.ui.tulostettuLabel.setText('Tulostettu')
+        self.ui.tulostettuLabel.setStyleSheet(u"color: rgb(0, 255, 0);")
+
+    # Avataan MessageBox
+    def openWarning(self):
+        msgBox = QtWidgets.QMessageBox()
+        msgBox.setIcon(QtWidgets.QMessageBox.Critical)
+        msgBox.setWindowTitle('Hirveetä!')
+        msgBox.setText('Jotian kamalaa tapahtui')
+        msgBox.setStandardButtons(QtWidgets.QMessageBox.Ok)
+        msgBox.exec()
 
 # Luodaan sovellus
 app = QtWidgets.QApplication(sys.argv)
